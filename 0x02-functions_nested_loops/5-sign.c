aadd6d9135117f86aae9b2115bd99197304b1ab4
COCOONKE <ngugzd@gmail.com>
2023-02-21 11:03:38 -0800

    task5 repeat

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * prints_sign - prints the sign of a number
- * @n: the number of the sign to be printed
- * Return: 1 if number is greater than zero
- * 0 if number is zero
- * -1 if number is less than zero
+ * print_sign - prints the sign of a number n
+ * @n: the number of the sign to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 int print_sign(int n)
 {
