085f39b2d1f730e2520d3e1bf3771568b14cf822
COCOONKE <ngugzd@gmail.com>
2023-02-24 14:51:29 -0800

    task00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,21 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - test function that prints if integer is positive or negative
- * Return: 0
- */
-
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+        return (0);
 }
